120f8c0ebb592febf32e6bf49fc302b98608266b
EyalWerber <79325403+EyalWerber@users.noreply.github.com>
2021-12-21 14:43:04 +0200

    markdown generater added

--- ECA_font_5_random_pillow.py
+++ ECA_font_5_random_pillow.py
@@ -1,128 +1,101 @@
-from os import replace
-import time
 import random
 
 
 def binasci_converter(rule):
 
-    return f'{rule:08b}'.replace('0',' ').replace('1','▓')
+    return f'{rule:08b}'.replace('0', ' ').replace('1', '▓')
 
-def rule_randomizer(rule,j,change_every):
-    if j%change_every==0:
-        rule=random.randint(1,255)
-        bin_rule = binasci_converter(rule)
-    
-        print('rule: ',rule)
-        return bin_rule,rule
-    else:
-        return binasci_converter(rule),rule
-       
 
-def rule_randomizer_bank(rule,j,change_every): #runs algorithm randomly through a bank of good chosen rules
-    if j%change_every==0:
-        rule_bank = [3,5,11,18,26,22,30,57,62,73,106,110,126,106,255,184,178,232,150,105,104,90,108,122,1,99,86]
-        rule=rule_bank[random.randint(0,len(rule_bank)-1)]
+# runs algorithm randomly through a bank of good chosen rules
+def rule_randomizer_bank(rule, j, change_every):
+    if j % change_every == 0:
+        rule = random.choice([3, 5, 11, 18, 26, 22, 30, 57, 62, 73, 106, 110,
+                             126, 106, 255, 184, 178, 232, 150, 105, 104, 90, 108, 122, 1, 99, 86])
         bin_rule = binasci_converter(rule)
-    
-        print('rule: ',rule)
-        return bin_rule,rule
+        with open('rules.txt', 'a') as f:
+            f.writelines(f'rule: {rule}\n')
+            
+        return bin_rule, rule
     else:
-        return binasci_converter(rule),rule
-        
+        return binasci_converter(rule), rule
 
 
 def zerolistmaker(n):
     listofzeros = ['.'] * n
     return listofzeros
 
-def rules(nums,rule):
-    if nums == [' ',' ',' ']:
+
+def rules(nums, rule):
+    if nums == [' ', ' ', ' ']:
         return rule[7]
-    elif nums==[' ',' ','▓']:
+    elif nums == [' ', ' ', '▓']:
         return rule[6]
-    elif nums==[' ','▓',' ']:
+    elif nums == [' ', '▓', ' ']:
         return rule[5]
-    elif nums==[' ','▓','▓']:
+    elif nums == [' ', '▓', '▓']:
         return rule[4]
-    elif nums==['▓',' ',' ']:
+    elif nums == ['▓', ' ', ' ']:
         return rule[3]
-    elif nums==['▓',' ','▓']:
+    elif nums == ['▓', ' ', '▓']:
         return rule[2]
-    elif nums==['▓','▓',' ']:
+    elif nums == ['▓', '▓', ' ']:
         return rule[1]
-    elif nums==['▓','▓','▓']:
+    elif nums == ['▓', '▓', '▓']:
         return rule[0]
-
-
 
 
 def singular_start(dots):
     return [' ' if not i == dots/2 else '▓' for i in range(dots)]
 
+
 def random_start(dots):
     return [random.choice(' ▓') for i in range(dots)]
-
-    
-
-
-
-
-
 
 
 def main(rule):
 
-    whole_prompt =[]
-    DOTS= 2020 #
-    
+    with open('rules.txt', 'w') as f:
+        pass
+
+    whole_prompt = []
+    DOTS = 2020
 
     # #  One starting point!
     gen_now = singular_start(DOTS)
 
-    #Random multiple starting points
+    # Random multiple starting points
     # gen_now=random_start(DOTS)
 
-    bin_rule = binasci_converter(rule)#format to 8-bit binary
+    bin_rule = binasci_converter(rule)  # format to 8-bit binary
 
-    for i in range(len(gen_now)):        
-            print(gen_now[i],end ='')
-    print('')
-
-
-    gen_next= zerolistmaker(len(gen_now))
-    iterations =3001
+    gen_next = zerolistmaker(len(gen_now))
+    iterations = 3001
 
     for j in range(iterations):
-        
-        gen_prev,gen_now = gen_now,gen_next
-        
-        for i in range(0,len(gen_now)):
-            
-            if i == 0:
-                
-                gen_next[i]= rules([' ',gen_prev[0],gen_prev[1]],bin_rule)
-            elif i ==len(gen_now)-1:
-                gen_next[i]= rules([gen_prev[i-1],gen_prev[i],' '],bin_rule)
 
+        gen_prev, gen_now = gen_now, gen_next
+
+        for i in range(0, len(gen_now)):
+
+            if i == 0:
+                gen_next[i] = rules([' ', gen_prev[0], gen_prev[1]], bin_rule)
+            elif i == len(gen_now)-1:
+                gen_next[i] = rules(
+                    [gen_prev[i-1], gen_prev[i], ' '], bin_rule)
 
             else:
                 rule_view = gen_prev[i-1:i+2]
-                gen_next[i] = rules(gen_prev[i-1:i+2],bin_rule)
+                gen_next[i] = rules(gen_prev[i-1:i+2], bin_rule)
 
-                # print(gen_prev[i-1:i+2])
-            
-        bin_rule,rule = rule_randomizer_bank(rule,j,200)   
-            
+        bin_rule, rule = rule_randomizer_bank(rule, j, 200)
+
         line = ''
-        
-        for i in range(len(gen_next)):        
-            line+=gen_next[i]
-        
-        gen_next= zerolistmaker(len(gen_now))
+
+        for i in range(len(gen_next)):
+            line += gen_next[i]
+
+        gen_next = zerolistmaker(len(gen_now))
         whole_prompt.append(line)
-        print('epoch: ',j)
-    
-    # input('Press sny bottun to exit!')
+        # print('epoch: ',j)
+
     return whole_prompt
-
-
--- script.py
+++ script.py
@@ -7,29 +7,6 @@ pingy = ECA_font_5_random_pillow.main(90)
 im= Image.new("RGB" ,(len(pingy[0]),len(pingy)),(0,0,0))
 draw=ImageDraw.Draw(im)
 
-def gradient_color(ECA_matrix):
-  sin_prox= 0
-  cos_prox = 0
-  gradient = 0
-  invert = ' '
-
-  for j in range(len(pingy[0])):
-    for i in range(len(pingy)):
-      if pingy[i][j] == invert: #'▓' or ' '
-
-        draw.point((j,i), fill=(int(255*math.e(sin_prox)),int(120*math.cos(cos_prox)),255*gradient))
-        if j<(len(pingy[0])/2):
-          gradient+=0.01
-        else:
-          invert = '▓'
-
-          gradient-=0.003
-        cos_prox += 0.0001
-
-      sin_prox += 0.1
-    print((j/len(pingy[0])*100),'%')
-
-
 def rules(ECA_matrix):
   invert = '▓'
 
@@ -45,11 +22,14 @@ def rules(ECA_matrix):
             draw.point((j,i), fill=(255,120,0))
         except IndexError as e:
           draw.point((j,i), fill=(255,0,0))
-    print((j/len(pingy[0])*100),'%')
+    # print((j/len(pingy[0])*100),'%')
 
+if __name__ == '__main__':
 
-rules(pingy)
-
-im.show()
-
-im.save("trial.png",format="PNG")
+  rules(pingy)
+  im.save("output.png",format="PNG")
+  with open('rules.txt','r') as f: 
+    prompt = f.read()
+  markdown = f"![photo](./output.png) \n {prompt}"
+  open("./README.md", "w", encoding="utf-8").write(markdown)
+  
